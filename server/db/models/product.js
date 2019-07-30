2b29587b90ad480bb5757bfcb628a83517124745
russellkerns <russellkerns@gmail.com>
2019-07-29 19:50:49 -0500

    added product and review models

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,2 +1,36 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: ''
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    defaultValue: 0,
+    validate: {
+      isNumeric: true
+    }
+  },
+  inventoryQuantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  }
+})
+
+module.exports = Product
--- server/db/models/review.js
+++ server/db/models/review.js
@@ -1,2 +1,21 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+
+const Review = db.define('review', {
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  rating: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 1,
+      max: 5
+    }
+  }
+})
+
+module.exports = Review;
