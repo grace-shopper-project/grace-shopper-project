738c10a6a76a4298ada16eeb9ff611d2fe14b9ae
Michelle Hoang <michellehoang.inbox@gmail.com>
2019-07-31 11:00:12 -0500

    navbar search added

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,45 +3,80 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
-import {Modal} from 'react-bootstrap'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div className="navHeader">
-    <h1
-      style={{
-        textAlign: 'center',
-        fontFamily: 'Corben, cursive',
-        fontSize: '3.5vw',
-        color: 'white',
-        fontWeight: 'underline',
-        padding: '1vw',
-        margin: '1vw'
-      }}
-    >
-      let's get this bread!
-    </h1>
-    {/* style={{backgroundColor: '#f2af1d'}} */}
+    <div className="bread">
+      <h1
+        style={{
+          textAlign: 'center',
+          fontFamily: 'Corben, cursive',
+          fontSize: '3.5vw',
+          color: 'black',
+          fontWeight: 'underline',
+          padding: '1vw',
+          margin: '1vw',
+          marginBottom: '3vw'
+        }}
+      >
+        let's get this bread!
+      </h1>
+    </div>
     <nav>
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/home">Products</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
+          <div style={{width: '25%'}} />
+          <div>
+            <div style={{width: '50%'}}>
+              <Link to="/home">Home</Link>
+            </div>
+            <div>
+              <Link to="/products">Products</Link>
+            </div>
+            <div>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </div>
+          </div>
         </div>
       ) : (
         <div>
+          <div style={{width: '30%'}} />
           {/* The navbar will show these links before you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/home">Products</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+          <div style={{width: '40%', justifyContent: 'center'}}>
+            <div>
+              <Link to="/home">Home</Link>
+            </div>
+            <div>
+              <Link to="/products">Products</Link>
+            </div>
+            <div>
+              <input
+                style={{
+                  justifyContent: 'left',
+                  width: '15vw',
+                  height: '1.75vw',
+                  marginTop: '0.5vw'
+                }}
+              />
+            </div>
+          </div>
+          <div style={{width: '30%', justifyContent: 'flex-end'}}>
+            <div>
+              <Link to="/login">Login</Link>
+            </div>
+            <div>
+              <Link to="/signup">Sign Up</Link>
+            </div>
+            <div style={{marginRight: '1.5vw'}}>
+              <Link to="/cart">Your Cart</Link>
+            </div>
+          </div>
         </div>
       )}
     </nav>
-    <hr />
   </div>
 )
 
