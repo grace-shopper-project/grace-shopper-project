9566e111251b1267755e9f8a0353212c83bb9add
Michelle Hoang <michellehoang.inbox@gmail.com>
2019-07-31 15:08:00 -0500

    all products view updated

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -2,30 +2,78 @@ import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import {fetchProducts} from '../store/allProducts.js'
 import ProductCard from './ProductCard'
+import {Dropdown, DropdownButton} from 'react-bootstrap'
 
 class AllProducts extends Component {
-  constructor() {
-    super()
-  }
+  // constructor() {
+  //   super()
+  // }
 
   componentDidMount() {
     this.props.fetchProducts()
   }
 
   render() {
-    if (!this.props.products.length) {
-      return <h1>LOADING......</h1>
-    } else
-      return (
-        <Fragment>
-          {this.props.products.map(product => {
-            const {imageUrl, name} = product
-            return (
-              <ProductCard key={product.id} imageUrl={imageUrl} name={name} />
-            )
-          })}
-        </Fragment>
-      )
+    return (
+      <div>
+        <div
+          style={{
+            width: '42vw',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+          }}
+        >
+          <div>
+            <DropdownButton
+              id="dropdown-item-button"
+              title="Filter by Category:"
+            >
+              <Dropdown.Item as="button" style={{width: '20vw'}}>
+                Action
+              </Dropdown.Item>
+              <Dropdown.Item as="button" style={{width: '20vw'}}>
+                Another action
+              </Dropdown.Item>
+              <Dropdown.Item as="button" style={{width: '20vw'}}>
+                Something else
+              </Dropdown.Item>
+            </DropdownButton>
+          </div>
+          <div>
+            <DropdownButton id="dropdown-item-button" title="Sort by:">
+              <Dropdown.Item as="button" style={{width: '20vw'}}>
+                Action
+              </Dropdown.Item>
+              <Dropdown.Item as="button" style={{width: '20vw'}}>
+                Another action
+              </Dropdown.Item>
+              <Dropdown.Item as="button" style={{width: '20vw'}}>
+                Something else
+              </Dropdown.Item>
+            </DropdownButton>
+          </div>
+        </div>
+        {!this.props.products.length ? (
+          <h1>LOADING......</h1>
+        ) : (
+          <div style={{width: '100vw'}}>
+            <div className="deck">
+              {this.props.products.map(product => {
+                const {imageUrl, name} = product
+                return (
+                  <ProductCard
+                    key={product.id}
+                    imageUrl={imageUrl}
+                    name={name}
+                  />
+                )
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+    )
   }
 }
 
