90d196cc6437c3e92c07e3a23b7dbd767efcdae4
Michelle Hoang <michellehoang.inbox@gmail.com>
2019-08-01 11:11:56 -0500

    connected single product view

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchSingleProduct} from '../store/singleProduct'
+
+export class SingleProduct extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Single Product</h1>
+      </div>
+    )
+  }
+}
+
+const mapState = state => ({
+  singleProduct: state.singleProduct
+})
+
+const mapDispatch = dispatch => {
+  return {fetchSingleProduct: () => dispatch(fetchSingleProduct())}
+}
+
+export default connect(mapState, mapDispatch)(SingleProduct)
