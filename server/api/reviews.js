75db6f194992d1ea542b14479622738a84c8263b
Madison Carr <madison.carr@utexas.edu>
2019-08-02 16:28:08 -0500

    fixed one more merge conflict

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-const reviewsRouter = require('express').Router()
-// const {Review, Product, User} = require('../db/models')
-
-// reviewsRouter.get('/', async (req, res, next) => {
-//   try {
-//     const reviews = await Review.findAll()
-//     if (reviews) {
-//       res.send(reviews)
-//     } else {
-//       res.sendStatus(500)
-//     }
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// reviewsRouter.get('/:reviewId', async (req, res, next) => {
-//   try {
-//     let reviewId = await Review.findByPk(req.params.reviewId, {
-//       include: [User, Product]
-//     })
-//     res.json(reviewId)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// reviewsRouter.post('/', async (req, res, next) => {
-//   try {
-//     let review = await Review.create(req.body, {returning: true})
-//     res.json(review)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// reviewsRouter.delete('/:reviewId', async (req, res, next) => {
-//   try {
-//     Review.destroy({
-//       where: {
-//         id: req.params.reviewId
-//       }
-//     })
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// reviewsRouter.put('/:reviewId', async (req, res, next) => {
-//   try {
-//     const review = await Review.findByPk(req.params.reviewId)
-//     if (!student) {
-//       return res.sendStatus(404)
-//     } else {
-//       let updateReview = review.update(req.body)
-//       res.json(updateReview)
-//     }
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-=======
 const reviewRouter = require('express').Router()
 const {Review, Product, User} = require('../db/models')
 //TODO: dont forget to add productId in the path
@@ -137,6 +74,5 @@ reviewRouter.put('/:reviewId', mustBeLoggedIn, async (req, res, next) => {
     next(err)
   }
 })
->>>>>>> 442757d5381ad4982356f131dde4b3e1a6a6cb9a
 
 module.exports = reviewRouter
