3e5c1781a21d28a3ce1bbd9fa284b5dafbd8d4ca
russellkerns <russellkerns@gmail.com>
2019-08-03 11:38:27 -0500

    working in cart api and store

--- server/api/cart.js
+++ server/api/cart.js
@@ -6,19 +6,21 @@ const User = require('../db/models/user')
 
 cartRouter.get('/', async (req, res, next) => {
   try {
+    let cart
     if (req.user) {
       const data = await Cart.findOrCreate({
         where: {userId: req.user.id},
         include: [Product, User]
       })
-      const cart = data[0].dataValues
+      cart = data[0].dataValues
     } else {
       const data = await Cart.findOrCreate({
         where: {sessionId: req.sessionID},
         include: [Product]
       })
-      const cart = data[0].dataValues
+      cart = data[0].dataValues
     }
+    res.json(cart)
   } catch (err) {
     next(err)
   }
@@ -26,34 +28,70 @@ cartRouter.get('/', async (req, res, next) => {
 
 cartRouter.put('/', async (req, res, next) => {
   try {
-    const cart = await Cart.findOne({
-      where: {
-        userId: req.body.user.id
-      }
-    })
-
-    if (req.body.event === 'add_to_cart') {
-      const cartDetails = await CartDetails.create({
+    let cart
+    if (req.user) {
+      cart = await Cart.findOne({
         where: {
-          cartId: cart.id
+          userId: req.body.user.id
         }
       })
-      const updatedCartDetails = await cartDetails.update({
-        productId: req.body.productId,
-        quantity: req.body.quantity
+    } else {
+      cart = await Cart.findOne({
+        where: {
+          sessionId: req.sessionID
+        }
       })
     }
+
+    const cartDetails = await CartDetails.findOne({
+      where: {
+        cartId: cart.id,
+        productId: req.body.productId
+      }
+    })
+    //if we want to update a product quantity or add a product
+    if (cartDetails) {
+      if (req.body.add) {
+        await cartDetails.update({
+          quantity: (cartDetails.quantity += req.body.quantity)
+        })
+      } else {
+        await cartDetails.update({
+          quantity: req.body.quantity
+        })
+      }
+    } else {
+      await cartDetails.create({
+        quantity: req.body.quantity,
+        cartId: cart.id,
+        productId: req.body.productId
+      })
+    }
+
+    //need to get the new cart
+    const updatedCart = await Cart.findByPk(cart.id, {
+      include: [Product]
+    })
+
+    res.json(updatedCart)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+cartRouter.delete('/:cartId', async (req, res, next) => {
+  try {
     const cartDetails = await CartDetails.findOne({
       where: {
         cartId: req.params.cartId,
         productId: req.body.productId
       }
     })
-    const updatedCartDetails = await cartDetails.update({
-      quantity: req.body.quantity
-    })
+    if (cartDetails) await cartDetails.destroy()
+    const updatedCart = await Cart.findByPk(req.params.cartId)
+    res.json(updatedCart)
   } catch (err) {
-    console.log(err)
+    next(err)
   }
 })
 
@@ -81,3 +119,20 @@ module.exports = cartRouter
 //   }
 //   res.json(cart)
 // }
+
+// came from put '/' route
+// if (req.body.event === 'add_to_cart') {
+//   const cartDetails = await CartDetails.create({
+//     where: {
+//       cartId: cart.id
+//     }
+//   })
+//   const updatedCartDetails = await cartDetails.update({
+//     productId: req.body.productId,
+//     quantity: req.body.quantity
+//   })
+// }
+
+// const updatedCartDetails = await cartDetails.update({
+//   quantity: req.body.quantity
+// })
