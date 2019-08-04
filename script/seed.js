dbac51dd0b8288e81c40e6b3fbf09ce9b3de950e
Madison Carr <madison.carr@utexas.edu>
2019-08-04 00:18:32 -0500

    adds search bar functionality

--- script/seed.js
+++ script/seed.js
@@ -15,16 +15,13 @@ async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-
-
-
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
     // fakerUsers.map(() => {}) // Do something here
   ])
   await Cart.create({total: 100, userId: 1})
-  const productArray = [
+  const productArray = await Promise.all([
     Product.create({
       name: 'Bagel',
       description:
@@ -282,8 +279,7 @@ async function seed() {
       price: 2.0,
       inventoryQuantity: 15
     })
-  ]
-
+  ])
 
   const reviewArray = [
     Review.create({
@@ -478,7 +474,7 @@ async function seed() {
 
   for (let i = 0; i < 995; i++) {
     productArray.push(
-      Product.create({
+      await Product.create({
         name: faker.random.word(),
         description: faker.lorem.paragraph(),
         imageUrl: '/images/bagel.png',
@@ -501,7 +497,7 @@ async function seed() {
 
   for (let i = 1; i < 40; i++) {
     reviewArray.push(
-      Review.create({
+      await Review.create({
         title: faker.random.word(),
         content: faker.lorem.paragraph(),
         rating: Math.ceil(Math.random() * Math.floor(5)),
@@ -522,9 +518,8 @@ async function seed() {
   }
   await Promise.all(productCategoryArray)
   await CartDetails.create({cartId: 1, orderId: 2, productId: 3, quantity: 2})
-await CartDetails.create({cartId: 1, orderId: 2, productId: 2, quantity: 2})
-await CartDetails.create({cartId: 1, orderId: 2, productId: 4, quantity: 2})
-
+  await CartDetails.create({cartId: 1, orderId: 2, productId: 2, quantity: 2})
+  await CartDetails.create({cartId: 1, orderId: 2, productId: 4, quantity: 2})
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
