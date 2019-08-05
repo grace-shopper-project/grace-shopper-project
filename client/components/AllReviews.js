66e1a216a4210655cc58d59aa05a64eb75dfb774
Kemi Thomas <kemothom71@gmail.com>
2019-08-04 21:17:05 -0500

    adding to the store

--- client/components/AllReviews.js
+++ client/components/AllReviews.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Card, CardDeck, Button} from 'react-bootstrap'
 import {Rater, Angry} from 'react-rater'
 import {fetchReviews, deleteReviews} from '../store/allReviews'
 
@@ -15,37 +16,74 @@ export class AllReviews extends React.Component {
 
     return (
       <div>
-        <header>Honest Reviews</header>
-        {reviews ? (
-          reviews.map(review => (
-            <div key={review.id}>
-              <p>{review.ratings}</p>
-              <Link to={`/reviews/${review.id}`}>
-                <h3>{review.title}</h3>
-              </Link>
-              <small>{new Date(review.createdAt).toDateString()}</small>
-              <small>{review.content}</small>
-            </div>
-          ))
-        ) : (
-          <div />
-        )}
         <div>
-          <p>
-            {/* {isAdmin ? (
-                <button
-                  type="button"
-                  onClick={e => {
-                    e.preventDefault()
-                    this.props.deleteReview(review.id)
+          <h1 style={{textAlign: 'center'}}>Honest Reviews</h1>
+        </div>
+        <div>
+          <CardDeck
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center'
+            }}
+          >
+            {reviews.map(review => (
+              <Card
+                key={review.id}
+                style={{
+                  border: '1px solid black',
+                  margin: '2vw',
+                  width: '60vw'
+                }}
+              >
+                <Card.Header
+                  style={{
+                    border: '1px solid black',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
                   }}
                 >
-                  X
-                </button>
-              ) : (
-                <div />
-              )} */}
-          </p>
+                  <div style={{margin: '1vw'}}>Item Name: </div>
+                  <div style={{margin: '1vw'}}>Rating: {review.rating}</div>
+                </Card.Header>
+                <Card.Body style={{border: '1px solid black'}}>
+                  <Card.Title>
+                    <h3>{review.title}</h3>
+                  </Card.Title>
+                  <Card.Text>{review.content}</Card.Text>
+                  {/* add link */}
+                  <button
+                    type="button"
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'center',
+                      margin: '1vw',
+                      border: '1px solid black',
+                      borderRadius: '15px'
+                    }}
+                  >
+                    Go somewhere
+                  </button>
+                </Card.Body>
+                <Card.Footer
+                  className="text-muted"
+                  style={{
+                    border: '1px solid black',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <div style={{width: '50%', margin: '1vw'}}>Written by:</div>
+                  <div style={{width: '50%', margin: '1vw'}}>
+                    Date of Review: {new Date(review.createdAt).toDateString()}
+                  </div>
+                </Card.Footer>
+              </Card>
+            ))}
+          </CardDeck>
         </div>
       </div>
     )
@@ -65,3 +103,43 @@ const mapDispatchToProps = dispatch => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllReviews)
+
+{
+  /* {reviews ? (
+          reviews.map(review => (
+            <div key={review.id}>
+              <p>{review.ratings}</p>
+              <Link to={`/reviews/${review.id}`}>
+                <h3>{review.title}</h3>
+              </Link>
+              <small>{new Date(review.createdAt).toDateString()}</small>
+              <small>{review.content}</small>
+            </div>
+          ))
+        ) : (
+          <div />
+        )} */
+}
+{
+  /* <div>
+          <p>
+            {/* {isAdmin ? (
+                <button
+                  type="button"
+                  onClick={e => {
+                    e.preventDefault()
+                    this.props.deleteReview(review.id)
+                  }}
+                >
+                  X
+                </button>
+              ) : (
+                <div />
+              )} */
+}
+{
+  /* </p> */
+}
+{
+  /* // </div> */
+}
--- client/store/orders.js
+++ client/store/orders.js
@@ -1,15 +1,57 @@
 import axios from 'axios'
 
 const RECIEVE_ORDER = 'RECIEVE_ORDER'
+const ADD_ORDER = 'ADD_ORDER'
+const DELETE_ORDER = 'DELETE_ORDER'
 
-export const setOrder = order => ({
+export const setOrder = orders => ({
   type: RECIEVE_ORDER,
+  orders
+})
+
+export const addOrder = order => ({
+  type: ADD_ORDER,
   order
 })
-const orderReducer = (state = [], action) => {
+
+export const fetchOrders = userId => {
+  return async dispatch => {
+    try {
+      const orderPath = `/api/users/${userId}`
+      const responses = await Promise.all([
+        axios.get(orderPath),
+        axios.get(`${orderPath}/orders`)
+      ])
+      const [users, orders] = responses.map(res => res.data)
+      users.orders = users
+      dispatch(setOrder(orders))
+    } catch (err) {
+      console.log("There's an error with fetchOrder")
+    }
+  }
+}
+
+export const newOrder = (userId, order) => {
+  return async dispatch => {
+    try {
+      const orderPath = `/api/users/${userId}`
+      const responses = await Promise.all([
+        axios.post(`${orderPath}/orders`, order)
+        axios.post(`/api/`) 
+      ])
+      const [users, orders] = responses.map(res => res.data)
+      users.orders = users
+      dispatch(addOrder(orders))
+    }catch(err) {
+      console.log("There's an error with newOrder")
+    }
+  }
+}
+
+export const orderReducer = (state = [], action) => {
   switch (action.type) {
     case RECIEVE_ORDER:
-      return action.order
+      return action.orders
     default:
       return state
   }
