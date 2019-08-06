479bbef4f675357ed18a6fe444c480f68bd725c5
russellkerns <russellkerns@gmail.com>
2019-08-06 16:27:47 -0500

    stripe is sort of working

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,30 +1,52 @@
 import React from 'react'
 import axios from 'axios'
 import StripeCheckout from 'react-stripe-checkout'
+import {toast} from 'react-toastify'
+import {connect} from 'react-redux'
 
-const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'https://get-this-bread-shop.herokuapp.com'
-  : 'https://localhost:8080';
+toast.configure()
 
+const PAYMENT_SERVER_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://get-this-bread-shop.herokuapp.com/api/stripe/checkout'
+    : 'http://localhost:8080/api/stripe/checkout'
 
-export default class Checkout extends React.Component {
-  onToken = (amount, description) =>  {
-    axios.post(PAYMENT_SERVER_URL, )
+class Checkout extends React.Component {
+  constructor(){
+    super()
+    this.handleToken = this.handleToken.bind(this)
+  }
+  async handleToken(token, addresses) {
+    const price = 15
+    const name = 'bread'
+    const product = {price, name}
+    const response = await axios.post(PAYMENT_SERVER_URL, {token, product})
+    const {status} = response.data
+    if (status === 'success') {
+      toast('Success! You cool person, check your email for details', {
+        type: 'success'
+      })
+    } else {
+      toast('Something wrong', {type: 'error'})
+    }
   }
 
   render() {
-    const {
-      firstName,
-      lastName,
-      streetAddress1,
-      streetAddress2,
-      city,
-      state,
-      zipCode,
-      phone
-    } = this.props.info
     return (
-      <StripeCheckout stripeKey="pk_test_f73ZiCX75vLgXX6P6EJC4jeU00O3LpELgt" />
+      <StripeCheckout
+        stripeKey="pk_test_f73ZiCX75vLgXX6P6EJC4jeU00O3LpELgt"
+        token={this.handleToken}
+        billingAddress
+        shippingAddress
+      />
     )
   }
 }
+
+const mapState = state => {
+  return {
+    product: state.cart
+  }
+}
+
+export default connect(mapState)(Checkout)
